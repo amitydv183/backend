8f3932ebe35795133f4f2b36b920db8b32e26f57
Amit Kumar <amkr9329@gmail.com>
2025-10-15 10:57:24 +0530

    user in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const UserModel = require("../models/User");
+const UserModel = require("../models/user");
 
 const checkAuth = async (req, res, next) => {
     const token = req.cookies.token; // cookie must be enabled
